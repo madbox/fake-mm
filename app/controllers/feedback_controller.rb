1b54553eb4d017071871e103551fbf56f7d7511e
madbox <mbarablin@locum.ru>
2010-05-01 19:12:36 +0400

    feedback beta

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -1,11 +1,18 @@
 class FeedbackController < ApplicationController
-  def new
-  end
 
   def create
-    Notifications.deliver_feedback_message( params[:name], params[:email], params[:text] )
-    flash[:notice] = I18n.t('notice.feedback.created')
+    fh = params[:feedback]
+    Notifications.deliver_feedback_message( fh[:name],
+                                            fh[:email],
+                                            fh[:text] )
+    if  fh[:name].blank? ||
+        fh[:email].blank? || !fh[:email].match() ||
+        fh[:text].blank?
+      flash[:error] = I18n.t( 'errors.incorrect_feedback_data' )
+      redirect_to :action => 'new'
+    end
+    
+    flash[:notice] = I18n.t('notice.feedback_created')
     redirect_to root_path
   end
-
 end
