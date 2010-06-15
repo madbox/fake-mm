b353db8a1e8cc65fa2316e120719350d8184526f
madbox <mbarablin@locum.ru>
2010-06-15 18:15:33 +0400

    environment.rb fix

--- config/environment.rb
+++ config/environment.rb
@@ -6,8 +6,6 @@ RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
-ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.merge!(:standard => "%d %b")
-
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
@@ -46,3 +44,5 @@ Rails::Initializer.run do |config|
   config.i18n.default_locale = :ru
 
 end
+
+ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.merge!(:standard => "%d %b")
